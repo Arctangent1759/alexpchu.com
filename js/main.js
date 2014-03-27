8276874c46c38b8d0377f965797d3b5371537a14
Alexander Chu <alex.p.chu@gmail.com>
2014-03-26 18:15:17 -0700

    update

--- js/main.js
+++ js/main.js
@@ -1,36 +1,58 @@
-var NUMCUBES=20;
+var NUMCUBES=50;
+var CUBE_DIMENSION=10;
+var CLIP_PADDING=2*CUBE_DIMENSION;
+var CLIP_PLANE=-100;
+
+var CUBE_X_VELOCITY=-2
+var CUBE_MAX_ROTATION=0.03
+
 
 $(document).ready(function(){
 
-/*
 
-  var scene = new THREE.Scene(); 
-  var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000); 
-  var camera = new THREE.OrthographicCamera( -window.innerWidth/2, window.innerWidth/2, window.innerHeight/2, -window.innerHeight/2, 0, 120 );
-  var renderer = new THREE.CanvasRenderer({canvas: $("#header_cvs").get(0), alpha:true});
-  renderer.setClearColor( 0xffffff, 0 );
+    var scene = new THREE.Scene(); 
+    var camera;
+    var renderer = new THREE.CanvasRenderer({canvas: $("#header_cvs").get(0), alpha:true});
+    renderer.setClearColor( 0x000000, 0);
 
-  $(window).resize(function(){
-    renderer.setSize(window.innerWidth, window.innerHeight); 
-  })
-  $(window).trigger('resize');
+    $(window).resize(function(){
+        renderer.setSize(window.innerWidth, window.innerHeight); 
+        camera = new THREE.OrthographicCamera( -window.innerWidth/2, window.innerWidth/2, window.innerHeight/2, -window.innerHeight/2, CLIP_PADDING, CLIP_PLANE-CLIP_PADDING );
+    })
+    $(window).trigger('resize');
 
-  var cube = getCube();
+    var cubes = [];
+    for (var i = 0; i < NUMCUBES; i++){
+        cubes[i] = [makeCube(),-1.0+CUBE_X_VELOCITY*Math.random(),[CUBE_MAX_ROTATION*(2*Math.random()-1),CUBE_MAX_ROTATION*(2*Math.random()-1),CUBE_MAX_ROTATION*(2*Math.random()-1)], [(window.innerWidth*2)*Math.random()+window.innerWidth/2,window.innerHeight*Math.random()-window.innerHeight/2,-CLIP_PLANE/2]];
+        cubes[i][0].position.x = cubes[i][3][0];
+        cubes[i][0].position.y = cubes[i][3][1];
+        cubes[i][0].position.z = cubes[i][3][2];
+        scene.add(cubes[i][0]);
+    }
 
-  scene.add(cube);
 
-  setInterval(function () { 
-    cube.rotation.x += 0.01; 
-    cube.rotation.y += 0.01; 
-    renderer.render(scene, camera);
-  },50);
-  */
+    setInterval(function () { 
+        renderer.render(scene, camera);
+        for (var i = 0; i < NUMCUBES; i++){
+
+            cubes[i][0].position.x += cubes[i][1];
+
+            cubes[i][0].rotation.x += cubes[i][2][0];
+            cubes[i][0].rotation.y += cubes[i][2][1];
+            cubes[i][0].rotation.z += cubes[i][2][2];
+
+            if (cubes[i][0].position.x < -window.innerWidth/2-CLIP_PADDING){
+                cubes[i][0].position.x = cubes[i][3][0];
+            }
+
+        }
+    },50);
 
 })
 
-function getCube(){
-  var cube = new THREE.BoxHelper();
-  cube.material.color.setRGB( 1, 0, 0 );
-  cube.scale.set( 10, 10, 10 );
-  return cube;
+function makeCube(){
+    var cube = new THREE.BoxHelper();
+    cube.material.color.setRGB( 1, 0, 0 );
+    cube.scale.set( CUBE_DIMENSION, CUBE_DIMENSION, CUBE_DIMENSION );
+    return cube;
 }
