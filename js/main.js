8188cd1a218a656f237d4f05a405d45df3db7043
Alexander Chu <alex.p.chu@gmail.com>
2014-03-27 06:21:27 -0700

    links finally work

--- js/main.js
+++ js/main.js
@@ -9,21 +9,64 @@ var CUBE_MAX_ROTATION=0.03
 
 $(document).ready(function(){
 
+    var cube_r=1;
+    var cube_g=0;
+    var cube_b=0;
+
+    $("#link_about").mouseover(function(){
+        cube_r=0;
+        cube_g=1;
+        cube_b=0;
+    });
+    $("#link_projects").mouseover(function(){
+        cube_r=0;
+        cube_g=0;
+        cube_b=1;
+    });
+    $("#link_github").mouseover(function(){
+        cube_r=1;
+        cube_g=1;
+        cube_b=0;
+    });
+    $("#link_resume").mouseover(function(){
+        cube_r=1;
+        cube_g=0;
+        cube_b=1;
+    });
+    $("#link_contact").mouseover(function(){
+        cube_r=0;
+        cube_g=1;
+        cube_b=1;
+    });
+
+    $("#title").mouseover(function(){
+        cube_r=1;
+        cube_g=0;
+        cube_b=0;
+    });
+
+
+    $(document).mousemove(function(event) {
+        camera.position.x=0.0000001*(event.pageX-window.innerWidth/2)*(event.pageX-window.innerWidth/2)*(event.pageX-window.innerWidth/2)
+        camera.position.y=-0.000001*(event.pageY-window.innerHeight/2)*(event.pageY-window.innerHeight/2)*(event.pageY-window.innerHeight/2)
+    });
+
+    function rescale(){
+        renderer.setSize(window.innerWidth, window.innerHeight); 
+        camera = new THREE.OrthographicCamera( -window.innerWidth/2, window.innerWidth/2, window.innerHeight/2, -window.innerHeight/2, -10000, 10000 );
+    }
 
     var scene = new THREE.Scene(); 
     var camera;
     var renderer = new THREE.CanvasRenderer({canvas: $("#header_cvs").get(0), alpha:true});
     renderer.setClearColor( 0x000000, 0);
 
-    $(window).resize(function(){
-        renderer.setSize(window.innerWidth, window.innerHeight); 
-        camera = new THREE.OrthographicCamera( -window.innerWidth/2, window.innerWidth/2, window.innerHeight/2, -window.innerHeight/2, CLIP_PADDING, CLIP_PLANE-CLIP_PADDING );
-    })
-    $(window).trigger('resize');
+    $(window).resize(rescale);
+    rescale();
 
     var cubes = [];
     for (var i = 0; i < NUMCUBES; i++){
-        cubes[i] = [makeCube(),-1.0+CUBE_X_VELOCITY*Math.random(),[CUBE_MAX_ROTATION*(2*Math.random()-1),CUBE_MAX_ROTATION*(2*Math.random()-1),CUBE_MAX_ROTATION*(2*Math.random()-1)], [(window.innerWidth*2)*Math.random()+window.innerWidth/2,window.innerHeight*Math.random()-window.innerHeight/2,-CLIP_PLANE/2]];
+        cubes[i] = [makeCube(),-1.0+CUBE_X_VELOCITY*Math.random(),[CUBE_MAX_ROTATION*(2*Math.random()-1),CUBE_MAX_ROTATION*(2*Math.random()-1),CUBE_MAX_ROTATION*(2*Math.random()-1)], [(window.innerWidth*2)*Math.random()+window.innerWidth/2,window.innerHeight*0.61803398875*Math.random()-window.innerHeight*0.61803398875/2,-CLIP_PLANE/2]];
         cubes[i][0].position.x = cubes[i][3][0];
         cubes[i][0].position.y = cubes[i][3][1];
         cubes[i][0].position.z = cubes[i][3][2];
@@ -34,6 +77,8 @@ $(document).ready(function(){
     setInterval(function () { 
         renderer.render(scene, camera);
         for (var i = 0; i < NUMCUBES; i++){
+
+            cubes[i][0].material.color.setRGB( cube_r, cube_g, cube_b );
 
             cubes[i][0].position.x += cubes[i][1];
 
@@ -48,6 +93,30 @@ $(document).ready(function(){
         }
     },50);
 
+    var SCROLLRATE = 1;
+
+    $(window).scroll(function(){
+        var perc = 25+75*(window.innerHeight-SCROLLRATE*$('body').scrollTop())/window.innerHeight;
+        if (perc < 0){
+            perc=0;
+        }
+        $("div#header").css({"height":perc +"%"});
+    });
+
+    $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                if (target.length) {
+                    var scroll = target.offset().top;// - $("div#header").height()
+                    $('html,body').animate({scrollTop: scroll}, 1000);
+                    return false;
+                }
+            }
+        });
+        //f(x) = x - 2*(h-x/h) = 
+    });
 })
 
 function makeCube(){
